2b2a3209fdbf5318f1c98a56e5a67bdbec2c2448
Majid AlHarbi <wbz054@gmail.com>
2025-08-17 17:20:47 +0300

    ن

---
+++ ai-cv-saudi-frontend/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>مرحباً بك في مشروعك Next.js!</h1>
+      <p>تم إعداد الصفحة الرئيسية بنجاح.</p>
+    </div>
+  );
+}
